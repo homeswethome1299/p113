73c46b5a10a0cbea641997cc8bacf098240e8494
homeswethome1299 <120109907+homeswethome1299@users.noreply.github.com>
2024-01-05 17:32:50 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+}
+
+function setup() {
+canvas = createCanvas(640,480);
+canvas.position(110,250);
+
+}
+
+function draw() {
+circle(320,300,200)
+circle(320,140,150)
+circle(240,80,80)
+circle(400,80,80)
+fill(165, 42, 42,)
+circle(400,360,100)
+circle(240,360,100)
+circle(240,260,90)
+circle(400,260,90)
+}
+
